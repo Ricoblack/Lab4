423bc33a5f625b9f6fc4b9679b4d076737942ae9
Renato Carella <renato.kinze@hotmail.it>
2016-06-10 22:50:52 +0200

    - teoricamente implementata activity notifiche

--- app/src/main/java/it/polito/mad/insane/lab4/activities/NotificationsActivity.java
+++ app/src/main/java/it/polito/mad/insane/lab4/activities/NotificationsActivity.java
@@ -2,8 +2,14 @@ package it.polito.mad.insane.lab4.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.ListView;
+
+import java.util.List;
 
 import it.polito.mad.insane.lab4.R;
+import it.polito.mad.insane.lab4.adapters.NotifyArrayAdapter;
+import it.polito.mad.insane.lab4.managers.RestaurateurJsonManager;
 
 public class NotificationsActivity extends AppCompatActivity {
 
@@ -12,7 +18,17 @@ public class NotificationsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.notifications_activity);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        ListView lv = (ListView) findViewById(R.id.notification_list_view);
+        if (lv != null){
+            RestaurateurJsonManager manager = RestaurateurJsonManager.getInstance(NotificationsActivity.this);
+            NotifyArrayAdapter adapter = new NotifyArrayAdapter(NotificationsActivity.this, R.layout.notify_listview_item,
+                    manager.getDailyOffersSimple());
+            lv.setAdapter(adapter);
+        }
     }
 }
--- app/src/main/java/it/polito/mad/insane/lab4/adapters/NotifyArrayAdapter.java
+++ app/src/main/java/it/polito/mad/insane/lab4/adapters/NotifyArrayAdapter.java
@@ -1,6 +1,7 @@
 package it.polito.mad.insane.lab4.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,7 +14,10 @@ import java.util.List;
 import it.polito.mad.insane.lab4.R;
 import it.polito.mad.insane.lab4.activities.MakeReservationActivity;
 import it.polito.mad.insane.lab4.activities.NotificationsActivity;
+import it.polito.mad.insane.lab4.activities.RestaurantProfileActivity;
+import it.polito.mad.insane.lab4.data.DailyOfferSimple;
 import it.polito.mad.insane.lab4.data.Dish;
+import it.polito.mad.insane.lab4.managers.RestaurateurJsonManager;
 
 /**
  * Created by Federico on 10/06/2016.
@@ -34,22 +38,37 @@ public class NotifyArrayAdapter extends ArrayAdapter<Dish> {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         View row = convertView;
         final NotifyHolder holder;
 
-        LayoutInflater inflater;
+        final LayoutInflater inflater;
         inflater = ((NotificationsActivity) context).getLayoutInflater();
         row = inflater.inflate(layoutResourceId, parent, false);
 
         holder = new NotifyHolder();
         holder.restaurantName = (TextView) row.findViewById(R.id.notify_item_restaurant_name);
-        holder.offerName = (TextView) row.findViewById(R.id.notify_item_offer_name);
         holder.description = (TextView) row.findViewById(R.id.notify_item_description);
         holder.notificationID = (TextView) row.findViewById(R.id.notify_item_notify_id);
         holder.cardview = (CardView) row.findViewById(R.id.notify_item_card_view);
 
+        holder.restaurantName.setText(((DailyOfferSimple) data.get(position)).getRestaurantName());
+        holder.description.setText(((DailyOfferSimple) data.get(position)).getDescription());
+        holder.notificationID.setText(((DailyOfferSimple) data.get(position)).getID());
 
+        holder.cardview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RestaurateurJsonManager manager = RestaurateurJsonManager.getInstance(context);
+                manager.removeDailyOffer(((DailyOfferSimple) data.get(position)).getID());
+
+                Intent resultIntent = new Intent(context, RestaurantProfileActivity.class);
+                resultIntent.putExtra("ID", ((DailyOfferSimple) data.get(position)).getID());
+                resultIntent.putExtra("Name",((DailyOfferSimple) data.get(position)).getRestaurantName());
+
+                context.startActivity(resultIntent);
+            }
+        });
 
         return row;
     }
@@ -57,7 +76,6 @@ public class NotifyArrayAdapter extends ArrayAdapter<Dish> {
     static class NotifyHolder
     {
         private TextView restaurantName;
-        private TextView offerName;
         private TextView notificationID;
         private TextView description;
         private CardView cardview;
