952901c0dcaf01d7b0b5dbb6d522f4eacee9e3e1
carlo <carlo.caramia@gmail.com>
2016-09-04 13:57:46 +0200

    richiesta permessi all'avvio

--- app/src/main/java/it/polito/mad/insane/lab4/activities/SplashScreenActivity.java
+++ app/src/main/java/it/polito/mad/insane/lab4/activities/SplashScreenActivity.java
@@ -1,9 +1,13 @@
 package it.polito.mad.insane.lab4.activities;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import it.polito.mad.insane.lab4.R;
 
@@ -17,23 +21,60 @@ public class SplashScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        new Handler().postDelayed(new Runnable() {
+
+        //Richiesta di permessi, inserire nel vettore di stringhe tutti i permessi necessari all'avvio dell'app.
+        //se anche uno di questi non è concesso l'app si chiude.
+        ActivityCompat.requestPermissions(SplashScreenActivity.this,
+                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION},
+                1);
+
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                    new Handler().postDelayed(new Runnable() {
 
             /*
              * Showing splash screen with a timer. This will be useful when you
              * want to show case your app logo / company
              */
 
-            @Override
-            public void run() {
-                // This method will be executed once the timer is over
-                // Start your app main activity
-                Intent i = new Intent(SplashScreenActivity.this, HomePageActivity.class);
-                startActivity(i);
+                        @Override
+                        public void run() {
+                            // This method will be executed once the timer is over
+                            // Start your app main activity
+                            Intent i = new Intent(SplashScreenActivity.this, HomePageActivity.class);
+                            startActivity(i);
 
-                // close this activity
-                finish();
+                            // close this activity
+                            finish();
+                        }
+                    }, SPLASH_TIME_OUT);
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(SplashScreenActivity.this, "Permissions denied", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                return;
             }
-        }, SPLASH_TIME_OUT);
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 }
